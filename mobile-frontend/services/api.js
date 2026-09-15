95625c1eb14bac5241b14964b6409404f1bafe85
Iniya-007 <iniyajaisankar07@gmail.com>
2026-09-15 11:24:38 +0530

    Configure production API for mobile app

--- mobile-frontend/services/api.js
+++ mobile-frontend/services/api.js
@@ -1,18 +1,7 @@
-import { Platform } from "react-native";
 import axios from "axios";
-import Constants from "expo-constants";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-let API_URL = "";
-
-if (Platform.OS === "web") {
-  // Running in browser
-  API_URL = "http://localhost:5000/api";
-} else {
-  // Running in Expo Go / Android
-  const host = Constants.expoConfig?.hostUri?.split(":")[0];
-  API_URL = `http://${host}:5000/api`;
-}
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
